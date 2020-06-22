9c04d38feb0c2816fc4c0cf0d0288bada2d2edbd
maryroselegaspi <maryrose.legasp2018@gmail.com>
2020-06-22 14:35:09 +1000

    add customer create modal component

--- OnboardingTask/ClientApp/src/components/Customer/Create.jsx
+++ OnboardingTask/ClientApp/src/components/Customer/Create.jsx
@@ -1,85 +1,134 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import { Modal, Form, Button, Header } from "semantic-ui-react";
+import { Modal, Table, Form, Button, Header, Icon } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
+import _ from 'lodash'
 
-export class Create extends Component{
+
+export class Customer extends Component
+{
+    _isMounted = false;
     constructor(props){
         super(props);
-
-        this.onChangeName = this.onChangeName.bind(this);
-        this.onChangeAddress = this.onChangeAddress.bind(this);
-        this.onCancel = this.onCancel.bind(this);
-        this.onSubmit = this.onSubmit.bind(this)
+        
 
         this.state = {
-            name: '',
-            address: '',
-            modalOpen: true,
+            customer: [],
+            loading: true,
+            failed: false,
+            id:0,
+            name:'',
+            address:'',
+            createshowModal: false,
+            error: '',
+            column: null,
+            direction: null, // should always be null
+
         }
+
+        
+    }
+    
+    //Connect  to the server
+    componentDidMount = (props)=> {
+       // props._isUnsubscribeAPi = true;
+        this._isMounted =true
+        this.populateCustomerData();
+    }
+    componentWillUnmount() {
+        this._isMounted =false
     }
 
-    onChangeName(e){
-        this.setState({
-            name: e.target.value
-            
-        });
+    // This will handle the input area to reflect the user's input value
+    onChangeName = (e) => this.setState({name: e.target.value }); 
+
+    onChangeAddress = (e) => { this.setState({ address: e.target.value});}
+
+    //Cancel the operation
+    onCancel = (e) => { 
+        this.setState({createshowModal:false, id:0, name:'', address:''})
     }
 
-    onChangeAddress(e){
-        this.setState({
-            address: e.target.value
-        });
-    }
-    onCancel(e){
-       
-        const {history} = this.props;
-        history.push('/store/store');
-        this.setState({modalOpen:false});
-    }
+    onCreateModalClose = () => this.setState({createshowModal:false})
 
-    onSubmit(e){
+    //Add new data
+    onCreate=(e)=>{
         e.preventDefault();
-        const {history} = this.props;
+        this.setState({createshowModal:false, name:'', address:''})
 
-        let customerObject = {
-            name: this.state.name,
-            address: this.state.address,      
+        let customerObject = { 
+            Name: this.state.name,
+            Address: this.state.address,      
         }
-
-        let res = "/api/customer/postcustomer";
-        axios.post(res, customerObject)
+    
+        axios.post("/api/customer/postcustomer", customerObject)      
+        this.componentDidUpdate();
+        //console.log(customerObject);
+    }
+  
+    //Fetch data from the backend
+     populateCustomerData(){
+        axios.get("/api/customer")
             .then(result => {
-                history.push('/customer/customer');
-        })
- 
-            .then(console.log({customerObject}));
-
+                if (this._isMounted){
+                    const response = result.data;
+                    this.setState({customer: response, loading: false, failed: false, error:""});
+                }
+            })
+            .catch(error => {
+                this.setState({customer: [], loading: false, failed: true, error:"Customer data could not be loaded"});
+            });       
     }
 
-    render(){
-        const {name, address} = this.state;
-        const {onCancel, onSubmit, onChangeName, onChangeAddress} = this;
-        return (
-            <div>
-               
-                    <Header content="Add Customer" />
-                    <Modal.Content>
-                        <Form >
-                            <Form.Input fluid label="Name" value={name} onChange={onChangeName}></Form.Input>
-                            <Form.Input fluid label="Address"  value={address} onChange={onChangeAddress}></Form.Input>
-                            
-                        </Form>
-                    </Modal.Content>
-                    <Modal.Actions>
-                        <Button color="black" onClick={onCancel}>cancel</Button>
-                        <Button color="green" onClick={onSubmit}> <i className="icon check" />Create</Button>
-                    </Modal.Actions>
-                
-            </div>
-         
-          
-        )
-    }  
-}
+    //Sorting -- Issue on 
+    handleSort = (clickedColumn) => {
+        const { customer, direction } = this.state
+        //console.log('last customer', customer) //remove this
+        this.setState({direction:'asc'})
 
+        let copydata = [...customer];
+
+        const sortedcustomer = (direction === 'asc')
+            ? _.orderBy(copydata, clickedColumn, 'asc')
+            : _.orderBy(copydata, clickedColumn, 'desc')
+
+        //console.log('sorting', sortedcustomer, clickedColumn, direction) //remove this
+
+        this.setState({ 
+            customer:sortedcustomer,
+            direction: direction === 'asc'? 'desc' : 'asc',
+            column:clickedColumn
+        })
+        //console.log('after setState', customer, direction) //remove this     
+    }
+   
+    render(){
+
+        const { direction, name, address, createshowModal } = this.state;
+        const { onChangeName, onChangeAddress, onCancel, onCreate} = this;
+          return (
+            <React.Fragment>
+               
+
+               {/* Create New customer Modal */}
+                <Modal size="small"   
+                        onClose={this.createshowModal} open={createshowModal} 
+                        trigger={<Button color="blue" onClick={() => this.setState({createshowModal:true})}>New Customer</Button>}   >
+                        <Header content="Create Customer" />
+                       
+                        <Modal.Content>
+                            <Form >
+                                <Form.Input  label="NAME" placeholder ='Name' value={name} onChange={(e) => onChangeName(e)}></Form.Input>
+                                <Form.Input  label="ADDRESS" placeholder ='Address' value={address} onChange={(e)=> onChangeAddress(e)}></Form.Input>
+                            </Form>
+                        </Modal.Content>
+                        <Modal.Actions>
+                            <Button color="black" onClick={() => onCancel()}>cancel</Button>
+                            <Button color="green" onClick={(e)=> onCreate(e)}> Create   <i className=" icon check" /></Button>
+                        </Modal.Actions>                   
+                </Modal>
+
+            </React.Fragment>
+        );
+    }
+}
