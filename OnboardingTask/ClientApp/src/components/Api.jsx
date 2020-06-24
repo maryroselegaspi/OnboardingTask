ed237b876f4b83dc147a15ef54a806ef6ec6f751
maryroselegaspi <maryrose.legasp2018@gmail.com>
2020-06-25 09:45:48 +1000

    added new api

--- OnboardingTask/ClientApp/src/components/Api.jsx
+++ OnboardingTask/ClientApp/src/components/Api.jsx
@@ -1,8 +1,9 @@
 
 const api ={
-    API_URL: "https://mvpreactshop.azurewebsites.net",
+    //API_URL: "https://mvpreactshop.azurewebsites.net",
     //API_URL: '',
     //API_URL: 'https://mvpshop.azurewebsites.net'
+    API_URL:"WEBSITE_WEBDEPLOY_USE_SCM",
 };
 
 export  { api }
