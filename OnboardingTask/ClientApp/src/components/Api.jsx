821e5852a2f9c885e2cdeb1f0cdffeff3f2a9820
maryroselegaspi <maryrose.legasp2018@gmail.com>
2020-06-26 19:07:36 +1000

    adding website url

--- OnboardingTask/ClientApp/src/components/Api.jsx
+++ OnboardingTask/ClientApp/src/components/Api.jsx
@@ -1,10 +1,10 @@
 
 const api ={
     //API_URL: "https://mvpreactshop.azurewebsites.net",
-   API_URL: '',
+   //API_URL: '',
     //API_URL:"https://mvpshop.azurewebsites.net",
     //API_URL:"https://onboardingmvp.azurewebsites.net",
-    //API_URL: "https://mvpaustraliaeast.azurewebsites.net/",
+    API_URL: "https://mvpaustraliaeast.azurewebsites.net/",
     
 };
 
