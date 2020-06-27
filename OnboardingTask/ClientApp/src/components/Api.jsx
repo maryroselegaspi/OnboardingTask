59bf8d90cb0c737d880efb963eac357102b7db54
maryroselegaspi <maryrose.legasp2018@gmail.com>
2020-06-27 22:45:46 +1000

    api change

--- OnboardingTask/ClientApp/src/components/Api.jsx
+++ OnboardingTask/ClientApp/src/components/Api.jsx
@@ -3,6 +3,6 @@ const api = {
     //API_URL: '',
     //API_URL: "https://mvpaustraliaeast.azurewebsites.net/",
     //API_URL: "https://mvpcentralus.azurewebsites.net/",
-    API_URL:"https://mvpshop.azurewebsites.net/",
+    API_URL:"https://mvpshop.scm.azurewebsites.net/",
 };
 export  { api }
