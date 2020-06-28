45230b25442c4c5223dd71b84010f9011c71f444
maryrose <maryrose.legasp2018@gmail.com>
2020-06-28 12:08:33 +1000

    remove unuse code

--- OnboardingTask/ClientApp/src/components/Api.jsx
+++ OnboardingTask/ClientApp/src/components/Api.jsx
@@ -1,22 +1,6 @@
 
-//import axios from 'axios';
-
-//export default axios.create({
-//        baseURL: ''
-//        //baseURL: "https://mvpaustraliaeast.azurewebsites.net/",
-//        //baseURL: "https://mvpcentralus.azurewebsites.net/",
-//        //API_URL: 'https://mvpshop.azurewebsites.net/',
-//    });
-
-
-
-    
-
-
 const api = {
-    API_URL: ''
-    //API_URL: "https://mvpaustraliaeast.azurewebsites.net/",
-    //API_URL: "https://mvpcentralus.azurewebsites.net/",
-    //API_URL:'https://mvpshop.azurewebsites.net/',
+    //API_URL: ''
+    API_URL:'https://mvpshop.azurewebsites.net/',
 };
 export  { api }
